425c3002bf957868b04b2ba22fa0a29342976ea8
vighnesh2005 <vighneshprasad12@gmail.com>
2025-06-13 16:43:17 +0530

    made dashboard

--- frontend/src/app/problems/page.jsx
+++ frontend/src/app/problems/page.jsx
@@ -1,11 +1,43 @@
-export default function Page(){
-    return(
-        <>
-            <div className="bg-gray-800 min-h-screen p-10 flex">
-                <div>
-                    
-                </div>
-            </div>
-        </>
-    )
-} 
+"use client";
+import Tags from "@/components/tags.jsx"; 
+import { useState } from "react";
+
+export default function TagBar() {
+    const tags = [
+    { name: "Array", count: 1931 },
+    { name: "String", count: 795 },
+    { name: "Hash Table", count: 704 },
+    { name: "Dynamic Programming", count: 594 },
+    { name: "Math", count: 588 },
+    { name: "Sorting", count: 456 },
+    { name: "Greedy", count: 418 },
+    { name: "Depth-First Search", count: 325 },
+    { name: "Binary Search", count: 300 },
+    { name: "Graph", count: 280 },
+    { name: "Backtracking", count: 200 },
+    { name: "Stack", count: 190 },
+    { name: "Two Pointers", count: 160 },
+    { name: "Trie", count: 150 },
+  ];
+
+  const [search,setSearch] = useState("");
+
+  return(
+    <>
+    <div className="p-4">
+        <Tags tags = {tags}></Tags>
+        
+        {/* search */}
+        <div className="flex flex-wrap gap-4 items-start">
+            <input className = "bg-slate-200 w-2/5 min-w-[20.5rem] my-5 p-2 rounded-md " 
+            type="text" placeholder="search a problem" value={search}
+            onChange={(e)=> setSearch(e.target.value) }
+            />
+            
+        </div>
+    </div>
+    </>
+
+  )
+   
+}
---
+++ frontend/src/components/tags.jsx
@@ -0,0 +1,46 @@
+"use client"
+import {useState} from 'react';
+
+export default function Tags(props){
+    const [expanded, setExpanded] = useState(false);
+      const initialCount = 8;
+    const {tags} = props;
+      return (
+        <div className="w-4/5 bg-black flex flex-wrap gap-3 rounded-lg p-4">
+            {
+                tags.slice(0,initialCount).map((tag,index)=>{
+                    return(
+                        <div key={index} className="text-white bg-gray-800 p-3 rounded-3xl hover:bg-gray-600 transition-all duration-100">
+                            {tag.name}
+                            <div className="inline-block bg-gray-900 rounded-2xl p-1 mx-2">{tag.count}</div>
+                        </div>
+                    )
+                })
+            }
+            {
+                expanded && tags.slice(initialCount).map((tag,index)=>{
+                    return(
+                        <div key={index} className="text-white bg-gray-800 p-3 rounded-3xl hover:bg-gray-600 transition-all duration-100">
+                            {tag.name}
+                            <div className="inline-block bg-gray-900 rounded-2xl p-1 mx-2">{tag.count}</div>
+                        </div>
+                    )
+                })
+            }
+            {
+                !expanded ? (
+                    <button onClick={() => setExpanded(true)}
+                    className="text-blue-600 p-3">
+                        expand +
+                    </button>
+                )
+                : (
+                    <button onClick={() => setExpanded(false)} 
+                    className="text-blue-600 p-3">
+                        collapse -
+                    </button>
+                )
+            }
+        </div>
+      ); 
+}
