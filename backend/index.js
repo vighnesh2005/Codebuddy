e6ca3e7d10ef2e7361c048ed803fbb412f94fb8c
vighnesh2005 <vighneshprasad12@gmail.com>
2025-06-30 17:42:06 +0530

    deployment success

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000","https://codebuddy-1wcn.vercel.app/"],
   credentials: true,
 }));
 app.use(express.json());
