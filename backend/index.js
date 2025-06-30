64c9e8d1f62732a86da108e4049ecfdd3d8a140a
vighnesh2005 <vighneshprasad12@gmail.com>
2025-06-30 16:14:57 +0530

    project almost complete

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   credentials: true,
 }));
 app.use(express.json());
@@ -31,9 +31,9 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 })
 
+const PORT = process.env.PORT || 4000;
 
-
-app.listen(4000,()=>{
+app.listen(PORT,()=>{
   console.log('Server is running on http://localhost:4000');
   connectDB();
 }); 
