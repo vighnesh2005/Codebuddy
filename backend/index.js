cee53e4e960b7d5410988543f95f704c2b0e8db0
vighnesh2005 <vighneshprasad12@gmail.com>
2025-06-30 16:37:06 +0530

    added origin localhost

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: (origin, callback) => {
-    callback(null, origin); // reflect the origin
-  },
+  origin: "http://localhost:3000",
   credentials: true,
 }));
 app.use(express.json());
