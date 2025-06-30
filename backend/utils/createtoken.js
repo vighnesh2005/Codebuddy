131a0918072b8790812e7a8edb9ca5606f732b14
vighnesh2005 <vighneshprasad12@gmail.com>
2025-06-30 17:00:12 +0530

    made https true

--- backend/utils/createtoken.js
+++ backend/utils/createtoken.js
@@ -11,7 +11,7 @@ export const createToken = (user,res) => {
     
     res.cookie('token', token, {
         httpOnly: true, 
-        secure: false,
+        secure: true,
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
     return token;
